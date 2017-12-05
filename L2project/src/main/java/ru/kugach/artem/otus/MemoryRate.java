f25a9d5f56ede287dedba53a2683701a6fc5985c
kuga14 <Artem.Kugach@gmail.com>
2017-12-06 00:09:11 +0300

    add constant variable

--- L2project/src/main/java/ru/kugach/artem/otus/MemoryRate.java
+++ L2project/src/main/java/ru/kugach/artem/otus/MemoryRate.java
@@ -5,33 +5,33 @@ import java.util.Collection;
 public class MemoryRate {
 
         private static final Runtime rt = Runtime.getRuntime ();
-        private static final int count = 1_000_000;
-        private static final int countCollection = 20;
+        private static final int COUNT = 1_000_000;
+        private static final int COUNT_COLLECTION = 20;
 
         private static long calcMemUsage(ObjectCreator factory) throws InterruptedException {
             Object handle;
             gc();
-            Object[] objects = new Object[count];
+            Object[] objects = new Object[COUNT];
             long mem1 = usedMemory();
-            for (int i = 0; i < count; i++) {
+            for (int i = 0; i < COUNT; i++) {
                 handle = factory.create();
                 objects[i] = handle;
             }
             gc();
             long mem2 = usedMemory();
-            for (int i = 0; i < count; i++) {
+            for (int i = 0; i < COUNT; i++) {
                 objects[i] = null;
             }
             objects=null;
-            return Math.round((float)(mem2 - mem1)/count);
+            return Math.round((float)(mem2 - mem1)/COUNT);
         }
 
         private static long collectionCalcMemUsage(ObjectCreator factory,int elements) throws InterruptedException {
             Collection handle=null;
-            Object[] objects = new Object[countCollection];
+            Object[] objects = new Object[COUNT_COLLECTION];
             gc();
             long mem1 = usedMemory();
-            for (int i = 0; i < countCollection; i++) {
+            for (int i = 0; i < COUNT_COLLECTION; i++) {
                 handle = (Collection) factory.create();
                 for(int j=0;j<elements;j++){
                     handle.add(new Integer(j+i));
@@ -42,11 +42,11 @@ public class MemoryRate {
             long mem2 = usedMemory();
             handle.clear();
             handle=null;
-            for (int i = 0; i < countCollection; i++) {
+            for (int i = 0; i < COUNT_COLLECTION; i++) {
                 objects[i]=null;
             }
             objects=null;
-            return Math.round((float)(mem2 - mem1)/countCollection);
+            return Math.round((float)(mem2 - mem1)/COUNT_COLLECTION);
         }
 
         public static String toPrintable(ObjectCreator factory) throws InterruptedException {
