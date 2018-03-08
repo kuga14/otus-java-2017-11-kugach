8a42d11af78547620491a4a98abd63e5bff6d17d
kuga14 <Artem.Kugach@gmail.com>
2018-03-08 19:59:41 +0300

    deleted unusable class ResultHendler

--- L9project/src/main/java/ru/kugach/artem/otus/executor/ResultHandler.java
+++
@@ -1,9 +0,0 @@
-package ru.kugach.artem.otus.executor;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-@FunctionalInterface
-public interface ResultHandler {
-    void handle(ResultSet result) throws SQLException;
-}
